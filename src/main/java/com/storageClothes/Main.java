d6e143b3d146061b4655cc8323328dd40509fe2a
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2020-09-09 18:36:19 -0300

    inclusão do repositório das roupas

--- src/main/java/com/storageClothes/Main.java
+++ src/main/java/com/storageClothes/Main.java
@@ -1,15 +1,36 @@
 package com.storageClothes;
 
+import com.storageClothes.domain.entities.Brand;
+import com.storageClothes.domain.entities.Clothes;
+import com.storageClothes.repository.ClothesRepository;
 import com.storageClothes.repository.DAO.ConnectionFactory;
+import com.storageClothes.utils.enuns.ColorEnum;
+import com.storageClothes.utils.enuns.SizeEnum;
 
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Connection con = new ConnectionFactory().getConnection();
-        System.out.println("Show");
-        con.close();
+    public static void main(String[] args) throws ParseException, FileNotFoundException {
+        Clothes c = new Clothes();
+        c.setCode(1);
+        c.setEntryDay("2020-10-10");
+        c.setBrand(new Brand());
+        c.setPaidPrice(1.11);
+        c.setSuggestedPrice(1.12);
+        c.setPriceTag(1.13);
+        c.setSize(SizeEnum.G);
+        c.setColor(ColorEnum.RED);
+
+        ClothesRepository cr = new ClothesRepository();
+        //cr.insert(c);
+        //Clothes cl = cr.getById(1);
+        cr.update(1,c);
+
+
     }
 }
--- src/main/java/com/storageClothes/repository/ClothesRepository.java
+++ src/main/java/com/storageClothes/repository/ClothesRepository.java
@@ -23,62 +23,101 @@ public class ClothesRepository {
         List<Clothes> list = new ArrayList<Clothes>();
 
         Scanner s = new Scanner(new File(FILE_NAME));
-        while (s.hasNext()){
-            String[] k = s.next().split("|");
+        while (s.hasNext()) {
+           String[] k = s.nextLine().split(";");
+            for(int i = 0;i<k.length;i++){
+                System.out.println(k[i]);
+            }
             Clothes c = new Clothes();
             c.setCode(Integer.valueOf(k[0]));
             c.setSize(SizeEnum.valueOf(k[7]));
-            c.setColor(ColorEnum.valueOf(k[8]));
+            c.setColor(ColorEnum.valueOf(k[6].trim()));
             c.setPriceTag(Double.valueOf(k[3]));
             c.setPaidPrice(Double.valueOf(k[4]));
             c.setSuggestedPrice(Double.valueOf(k[5]));
 
             BrandRepository b = new BrandRepository();
-            c.setBrand(b.getById(Integer.valueOf(k[2])));
-
-            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(k[1]);
-            c.setEntryDay(date);
+            //c.setBrand(b.getById(Integer.valueOf(k[2])));
+            c.setEntryDay(k[1]);
             list.add(c);
         }
 
         s.close();
         return list;
     }
-    public Clothes getById(int id){
 
-        Clothes c = new Clothes();
-        String sql = "select * from clothes where code = " + id;
+    public Clothes getById(int id) throws FileNotFoundException, ParseException {
 
-            return c;
+        List<Clothes> l = listAll();
+        Clothes c = null;
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getCode() == id) {
+                c = l.get(i);
+                break;
+            }
+        }
+        return c;
+
+    }
+
+    public void deleteById(int id) throws FileNotFoundException, ParseException {
+        List<Clothes> l = listAll();
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getCode() == id) {
+                l.remove(id);
+                break;
+            }
+        }
+        deleteFile();
+        for (int i = 0; i < l.size(); i++) {
+            insert(l.get(i));
+        }
 
 
     }
 
-    public void deleteById(int id){
-        String sql = "delete from clothes where code = " + id;
+    public void update(int id, Clothes clothes) throws FileNotFoundException, ParseException {
+        List<Clothes> l = listAll();
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).getCode() == id) {
+                l.remove(id);
+                break;
+            }
+        }
+        l.add(clothes);
+        deleteFile();
+        for (int i = 0; i < l.size(); i++) {
+            insert(l.get(i));
+        }
 
     }
 
-    public void update(int id,Clothes clothes){
+    public void insert(Clothes clothe) {
+        String c = clothe.getCode() + ";" +
+                clothe.getEntryDay() + ";" +
+                clothe.getBrand().getId() + ";" +
+                clothe.getPriceTag() + ";" +
+                clothe.getPaidPrice() + ";" +
+                clothe.getSuggestedPrice() + ";" +
+                clothe.getColor() + ";" +
+                clothe.getSize().getSizeName() + "\n";
 
-    }
-    public void insert(Clothes clothe){
-        String c = clothe.getCode()+"|"+
-                clothe.getEntryDay().toString()+"|"+
-                clothe.getBrand().getId()+"|"+
-                clothe.getPriceTag()+"|"+
-                clothe.getPaidPrice()+"|"+
-                clothe.getSuggestedPrice()+"|"+
-                clothe.getColor().getColorName()+"|"+
-                clothe.getSize().getSizeName()+"\n";
-
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME,true));
             writer.write(c);
 
             writer.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void deleteFile() {
+        try {
+            File f = new File(FILE_NAME);
+            f.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
